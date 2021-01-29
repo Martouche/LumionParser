3f6b22ea2e738d0915f4eef4b0bfe43e3899b044
Martin <martin.vantalon@epitech.eu>
2021-01-29 14:45:20 +0100

    [+] basics [+]

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LumionParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
