2e60cf110a79c641e48b9f04c1ae4f546fb70c42
Martin <martin.vantalon@epitech.eu>
2021-02-11 14:52:07 +0100

    almost done parser

--- LumionParser/Form1.cs
+++ LumionParser/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,10 +10,12 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace LumionParser
 {
     public partial class LumionParserDxdiag : Form
     {
+        public bool frenchisclicked = false;
         public LumionParserDxdiag()
         {
             InitializeComponent();
@@ -32,32 +35,59 @@ namespace LumionParser
         {
             richTextBoxdisplay.Clear();
             string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
-            foreach (string file in files) {
-                MessageBox.Show(file, "form test", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            ///            foreach (string file in files) {
+            ///                MessageBox.Show(file, "form test", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            ///            }
+            if (Path.GetExtension(files[files.Length - 1]) != ".txt" && frenchisclicked == false)
+                MessageBox.Show("Wrong extension file \nOnly accept .txt file", "Error : Wrong extension", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            else if (Path.GetExtension(files[files.Length - 1]) != ".txt" && frenchisclicked == true)
+                MessageBox.Show("Mauvaise extension de fichier\nseulement fichier .txt accepté", "Erreur : Mauvaise extension", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            else
+            {
+                parserhandler(File.ReadAllText(files[files.Length - 1]));
             }
-            string readText = File.ReadAllText(files[files.Length - 1]);
-            List<string> result = readText.Split('\n').ToList();
-            getgoodlines(result);
         }
 
-        void getgoodlines(List<string> fulldatas)
+        void parserhandler(string text)
+        {
+            string readText = text;
+            List<string> first = readText.Split('\n').ToList();
+            List<string> second = getgoodlines(first);
+            List<string> third = cleanlines(second);
+            foreach (string lines in third)
+                richTextBoxdisplay.AppendText(lines);
+
+        }
+
+        List<string> cleanlines(List<string> fulldatas)
+        {
+            var good = new List<string>();
+            foreach (string data in fulldatas)
+            {
+                good.Add(data.Trim() + '\n');
+            }
+            return good;
+        }
+
+        List<string> getgoodlines(List<string> fulldatas)
         {
             var gooddatas = new List<string>();
             StringComparison comp = StringComparison.OrdinalIgnoreCase;
             foreach (string data in fulldatas) {
                 if (data.Contains("Operating System", comp) == true)
-                    richTextBoxdisplay.AppendText(data);
+                    gooddatas.Add(data);
                 else if (data.Contains("Card name", comp) == true)
-                    richTextBoxdisplay.AppendText(data);
+                    gooddatas.Add(data);
                 else if (data.Contains("  Memory", comp) == true)
-                    richTextBoxdisplay.AppendText(data);
+                    gooddatas.Add(data);
                 else if (data.Contains("Driver Date/size", comp) == true)
-                    richTextBoxdisplay.AppendText(data);
+                    gooddatas.Add(data);
                 else if (data.Contains("Page File", comp) == true)
-                    richTextBoxdisplay.AppendText(data);
+                    gooddatas.Add(data);
                 else if (data.Contains("Free Space", comp) == true)
-                    richTextBoxdisplay.AppendText(data);
+                    gooddatas.Add(data);
             }
+            return gooddatas;
         }
 
         private void buttonfrench_Click(object sender, EventArgs e)
@@ -65,6 +95,7 @@ namespace LumionParser
             labeldragdrop.Text = "Glissez votre DxDiag ici !";
             Englishbutton.Text = "Anglais";
             buttonfrench.Text = "Français";
+            frenchisclicked = true;
         }
 
         private void buttonenglish_Click(object sender, EventArgs e)
@@ -72,6 +103,7 @@ namespace LumionParser
             labeldragdrop.Text = "Slide your DxDiag here !";
             buttonfrench.Text = "French";
             Englishbutton.Text = "English";
+            frenchisclicked = false;
         }
     }
 }
