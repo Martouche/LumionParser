a5c85b8c94bb9a3158cbe2aa1f8e937d3e2fe117
Martin Vantalon <martin.vantalon@epitech.eu>
2021-02-11 09:15:31 +0100

    Add files via upload

---
+++ LumionParser/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace LumionParser
+{
+    partial class LumionParserDxdiag
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LumionParserDxdiag));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Englishbutton = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.richTextBoxdisplay = new System.Windows.Forms.RichTextBox();
+            this.labeldragdrop = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonfrench = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonfrench);
+            this.panel1.Controls.Add(this.Englishbutton);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(178, 893);
+            this.panel1.TabIndex = 0;
+            // 
+            // Englishbutton
+            // 
+            this.Englishbutton.BackColor = System.Drawing.Color.Teal;
+            this.Englishbutton.ForeColor = System.Drawing.Color.White;
+            this.Englishbutton.Location = new System.Drawing.Point(0, 248);
+            this.Englishbutton.Name = "Englishbutton";
+            this.Englishbutton.Size = new System.Drawing.Size(178, 69);
+            this.Englishbutton.TabIndex = 1;
+            this.Englishbutton.Text = "English";
+            this.Englishbutton.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(178, 229);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.richTextBoxdisplay);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(178, 396);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1538, 497);
+            this.panel3.TabIndex = 1;
+            // 
+            // richTextBoxdisplay
+            // 
+            this.richTextBoxdisplay.BackColor = System.Drawing.Color.Teal;
+            this.richTextBoxdisplay.ForeColor = System.Drawing.Color.White;
+            this.richTextBoxdisplay.Location = new System.Drawing.Point(30, 38);
+            this.richTextBoxdisplay.Name = "richTextBoxdisplay";
+            this.richTextBoxdisplay.Size = new System.Drawing.Size(1483, 436);
+            this.richTextBoxdisplay.TabIndex = 0;
+            this.richTextBoxdisplay.Text = "";
+            // 
+            // labeldragdrop
+            // 
+            this.labeldragdrop.AutoSize = true;
+            this.labeldragdrop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labeldragdrop.ForeColor = System.Drawing.Color.White;
+            this.labeldragdrop.Location = new System.Drawing.Point(730, 73);
+            this.labeldragdrop.Name = "labeldragdrop";
+            this.labeldragdrop.Size = new System.Drawing.Size(455, 54);
+            this.labeldragdrop.TabIndex = 3;
+            this.labeldragdrop.Text = "Glissez votre DxDiag ici !";
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.BackColor = System.Drawing.Color.Teal;
+            this.listBox1.ForeColor = System.Drawing.Color.Teal;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 41;
+            this.listBox1.Location = new System.Drawing.Point(328, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1260, 250);
+            this.listBox1.TabIndex = 4;
+            // 
+            // buttonfrench
+            // 
+            this.buttonfrench.BackColor = System.Drawing.Color.Teal;
+            this.buttonfrench.ForeColor = System.Drawing.Color.White;
+            this.buttonfrench.Location = new System.Drawing.Point(0, 323);
+            this.buttonfrench.Name = "buttonfrench";
+            this.buttonfrench.Size = new System.Drawing.Size(178, 69);
+            this.buttonfrench.TabIndex = 2;
+            this.buttonfrench.Text = "French";
+            this.buttonfrench.UseVisualStyleBackColor = false;
+            // 
+            // LumionParserDxdiag
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(1716, 893);
+            this.Controls.Add(this.labeldragdrop);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LumionParserDxdiag";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LumionDxDiagParser";
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Englishbutton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox richTextBoxdisplay;
+        private System.Windows.Forms.Label labeldragdrop;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonfrench;
+    }
+}
+
---
+++ LumionParser/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LumionParser
+{
+    public partial class LumionParserDxdiag : Form
+    {
+        public LumionParserDxdiag()
+        {
+            InitializeComponent();
+            buttonfrench.Click += buttonfrench_Click;
+            Englishbutton.Click += buttonenglish_Click;
+            listBox1.AllowDrop = true;
+            listBox1.DragEnter += new DragEventHandler(Form1_DragEnter);
+            listBox1.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            richTextBoxdisplay.Clear();
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files) {
+                MessageBox.Show(file, "form test", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            string readText = File.ReadAllText(files[files.Length - 1]);
+            List<string> result = readText.Split('\n').ToList();
+            getgoodlines(result);
+        }
+
+        void getgoodlines(List<string> fulldatas)
+        {
+            var gooddatas = new List<string>();
+            StringComparison comp = StringComparison.OrdinalIgnoreCase;
+            foreach (string data in fulldatas) {
+                if (data.Contains("Operating System", comp) == true)
+                    richTextBoxdisplay.AppendText(data);
+                else if (data.Contains("Card name", comp) == true)
+                    richTextBoxdisplay.AppendText(data);
+                else if (data.Contains("  Memory", comp) == true)
+                    richTextBoxdisplay.AppendText(data);
+                else if (data.Contains("Driver Date/size", comp) == true)
+                    richTextBoxdisplay.AppendText(data);
+                else if (data.Contains("Page File", comp) == true)
+                    richTextBoxdisplay.AppendText(data);
+                else if (data.Contains("Free Space", comp) == true)
+                    richTextBoxdisplay.AppendText(data);
+            }
+        }
+
+        private void buttonfrench_Click(object sender, EventArgs e)
+        {
+            labeldragdrop.Text = "Glissez votre DxDiag ici !";
+            Englishbutton.Text = "Anglais";
+            buttonfrench.Text = "Français";
+        }
+
+        private void buttonenglish_Click(object sender, EventArgs e)
+        {
+            labeldragdrop.Text = "Slide your DxDiag here !";
+            buttonfrench.Text = "French";
+            Englishbutton.Text = "English";
+        }
+    }
+}
